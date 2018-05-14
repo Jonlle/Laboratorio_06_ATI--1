0f638a058687a3e0f003d5a67e37be40fb9bad5e
JenniC <jen_cadiz@hotmail.com>
2018-05-14 18:53:35 -0400

    Segunda modificacion en el js

--- jq.js
+++ jq.js
@@ -46,13 +46,13 @@ var loadDoc = function() {
 function myFunction( xml ) {
 	var i;
 	var xmlDoc = $.parseXML(xml);
-	var table="<tr><th>Artista</th><th>Titulo</th></tr>";
+	var table="<tr><th>Artista</th><th>Titulo</th><th>Año</th></tr>";
 	$("#demo").empty();
 
 	$xml = $( xmlDoc );
 	$(xml).find("CD").each(function() {
 		table += "<tr><td>" + $(this).find("ARTIST").text() + "</td><td>" + 
-		$(this).find("TITLE").text() + "</td></tr>";
+		$(this).find("TITLE").text() + "</td><td>" + $(this).find("YEAR").text() + "</td></tr>";
 	});
 	$("#demo").html(table);
 }
